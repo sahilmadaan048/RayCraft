9d7bfb32524def72dd726c36e6e75d9e069fb156
Sahil Madaan <madaan.sahil27@gmail.com>
2025-04-15 17:25:21 +0530

    🐼 project initislised and irfanView installed

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    //image
+    
+    const int image_width = 256;
+    const int image_height = 256;
+    
+    //render
+    cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+
+    for(int i = image_height-1; i>=0; i--) {
+        for(int j=0; j<image_width; j++) {
+            auto r = double(i) / (image_height - 1);
+            auto g = double(j) / (image_height - 1);
+            auto b = 0.25;
+
+            int ir = static_cast<int> (255.999 * r);
+            int ig = static_cast<int> (255.990 * g);
+            int ib = static_cast<int> (255.999 * b);
+        
+            cout << ir << ' ' << ig << ' ' << ib << '\n';
+        }
+    }
+    cerr << "\nDone.\n";
+}
