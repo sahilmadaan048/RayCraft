9b6b5137f440a3ab99d717be8f8b910eed2b88fd
Sahil Madaan <madaan.sahil27@gmail.com>
2025-04-18 19:32:26 +0530

    progess tracker updated

--- main.cpp
+++ main.cpp
@@ -9,22 +9,21 @@ int main() {
     
     //render
     cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+    for (int j = 0; j < image_height; ++j) {
+        std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
+        for (int i = 0; i < image_width; i++) {
+            auto r = double(i) / (image_width-1);
+            auto g = double(j) / (image_height-1);
+            auto b = 0.0;
 
-    for(int i = image_height-1; i>=0; i--) {
-        clog << "\rScanliness remaining: " << (image_height - i) << flush;
-        for(int j=0; j<image_width; j++) {
-            auto r = double(i) / (image_height - 1);
-            auto g = double(j) / (image_height - 1);
-            auto b = 0.25;
+            int ir = int(255.999 * r);
+            int ig = int(255.999 * g);
+            int ib = int(255.999 * b);
 
-            int ir = static_cast<int> (255.999 * r);
-            int ig = static_cast<int> (255.990 * g);
-            int ib = static_cast<int> (255.999 * b);
-        
-            cout << ir << ' ' << ig << ' ' << ib << '\n';
+            std::cout << ir << ' ' << ig << ' ' << ib << '\n';
         }
     }
-    cerr << "\nDone.\n";
+
+    std::clog << "\rDone.                 \n";
 }
 
-//just goofing around and testing irfanView for multiplle values of width and height
